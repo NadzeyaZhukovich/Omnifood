01f98045ce661174d8d79863d1c764a79a9734d1
Nadzeya <naduva0505@gmail.com>
2019-03-07 22:56:11 +0100

    Added scroll on buttons and navigation scroll with Jquery

--- omnifood/recources/js/script.js
+++ omnifood/recources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if(direction == "down") {
             $('nav').addClass('sticky');
@@ -6,6 +8,31 @@ $(document).ready(function() {
             $('nav').removeClass('sticky');
         }
     }, {
-        offeset: '60px;' 
+        offset: '60px;' 
     });
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /*Navigation scroll*/
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
 });
